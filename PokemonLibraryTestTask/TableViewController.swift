e27e889b8b9c79ac256b6657a744db75011bb954
Valentin Vafiadi <vafiadiv@gmail.com>
2021-01-24 23:45:40 +0300

    add review comments

--- PokemonLibraryTestTask/TableViewController.swift
+++ PokemonLibraryTestTask/TableViewController.swift
@@ -8,8 +8,12 @@
 import UIKit
 import SwiftGifOrigin
 
+//Лучше в названии класса указывать, за что он отвечает, например PokemonTableViewController. Хотя тут маленькая прила, ничего
+//кроме покемонов нет, но это хорошая привычка. Посмотри, в каких ещё классах это применить стоит
 class TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+  //про public и private на курсе должны были рассказывать. Часть полей ниже должна быть помечена как public или private,
+  //почитай или погугли что тут должно быть, и добавь
   let loadingGif = UIImage.gif(name: "loading")
   var url = URL(string: "https://pokeapi.co/api/v2/pokemon?limit=30&offset=0")!
   let pokemonTableView = UITableView()
@@ -17,26 +21,33 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
   var pokemonList = [Pokemon]()
   var fetchingMore = false
   var counter = 30
-  
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.addSubview(pokemonTableView)
-        self.navigationItem.title = "Pokemons";
-      
-        pokemonTableView.register(UITableViewCell.self, forCellReuseIdentifier: pokemonCell)
-        pokemonTableView.register(LoadingCell.self, forCellReuseIdentifier: LoadingCell.loadingCell)
-      
-        pokemonTableView.delegate = self
-        pokemonTableView.dataSource = self
-      
-        pokemonTableView.translatesAutoresizingMaskIntoConstraints = false
-        pokemonTableView.topAnchor.constraint(equalTo:view.topAnchor).isActive = true
-        pokemonTableView.leftAnchor.constraint(equalTo:view.leftAnchor).isActive = true
-        pokemonTableView.rightAnchor.constraint(equalTo:view.rightAnchor).isActive = true
-        pokemonTableView.bottomAnchor.constraint(equalTo:view.bottomAnchor).isActive = true
-        pokemonTableView.tableFooterView = UIView() // hid empty cells
-        downloadJson()
-    }
+
+  //отступы уехали. Xcode их может разом поправить, погугли как
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    //ниже в этом методе идёт код, который отвечает за разное: один - за контент таблицы (регистрация ячеек,
+    //проставление dataSource, delegate) а другой - за лейаут (создание constraint'ов, добавление subview). Лучше такие вещи разделять.
+    //У ViewController'ов можно задавать cвой кастомный класс View. Давай сделаем, чтоб всё что относится к лейауту было
+    //в отдельном классе, PokemonTableView и он проставлялся этому ViewController'у. Тоже, погугли как это делается в методе loadView(),
+    //если на курсах не проходили
+    view.addSubview(pokemonTableView)
+    self.navigationItem.title = "Pokemons";
+
+    pokemonTableView.register(UITableViewCell.self, forCellReuseIdentifier: pokemonCell)
+    pokemonTableView.register(LoadingCell.self, forCellReuseIdentifier: LoadingCell.loadingCell)
+
+    pokemonTableView.delegate = self
+    pokemonTableView.dataSource = self
+
+    pokemonTableView.translatesAutoresizingMaskIntoConstraints = false
+    pokemonTableView.topAnchor.constraint(equalTo:view.topAnchor).isActive = true
+    pokemonTableView.leftAnchor.constraint(equalTo:view.leftAnchor).isActive = true
+    pokemonTableView.rightAnchor.constraint(equalTo:view.rightAnchor).isActive = true
+    pokemonTableView.bottomAnchor.constraint(equalTo:view.bottomAnchor).isActive = true
+    pokemonTableView.tableFooterView = UIView() // hid empty cells
+    downloadJson()
+  }
 
   func downloadJson() {
     URLSession.shared.dataTask(with: url, completionHandler: { [weak self] data, response, error in
@@ -55,10 +66,11 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
       }
     }).resume()
   }
-  
+
   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     tableView.deselectRow(at: indexPath, animated: true)
     let vc = ViewController()
+    //force-unwrap - это небезопасно: если значение будет nil, приложение упадёт. Надо от него избавиться тут, есть пара способов
     var currentSelectedImage: URL!
     var currentSelectedLabel: String!
     currentSelectedImage = pokemonList[indexPath.row].imageURL
@@ -67,9 +79,9 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
     vc.setLabelName(name: currentSelectedLabel)
     self.navigationController?.pushViewController(vc, animated: true)
   }
-  
+
   func numberOfSections(in tableView: UITableView) -> Int {
-     return 2
+    return 2
   }
 
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -79,7 +91,7 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
       return 1
     }
     return 0
- }
+  }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     if indexPath.section == 0 {
@@ -87,15 +99,16 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
       cell.accessoryType = .disclosureIndicator
       cell.textLabel?.text = pokemonList[indexPath.row].name.capitalized
       cell.imageView?.kf.setImage(with: pokemonList[indexPath.row].imageURL, placeholder: loadingGif)
-     return cell
+      return cell
     } else {
       let cell = tableView.dequeueReusableCell(withIdentifier: LoadingCell.loadingCell, for: indexPath) as! LoadingCell
+      //Такой способ есть, но он не очень хороший, т.к. разделитель всё равно есть. Лучше сделай separator прозрачным
       cell.separatorInset = UIEdgeInsets(top: 0, left: 10000, bottom: 0, right: 0) // hid separator line
       cell.loadingIndicator.startAnimating()
-     return cell
+      return cell
     }
- }
-    
+  }
+
   func scrollViewDidScroll(_ scrollView: UIScrollView) {
     let offsetY = scrollView.contentOffset.y
     let contentHeight = scrollView.contentSize.height
@@ -106,11 +119,14 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
       }
     }
   }
-  
+
   func beginBatchFeatch() {
     fetchingMore = true
     pokemonTableView.reloadSections(IndexSet(integer: 1), with: .none)
     DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+      //Получается, что у нас грузится всегда полный список, с 0го покемона. Сначала 30, потом 60, потом 90 и т.д. Избыточно получается.
+      //Тут в URL'е есть как раз параметр offset - с какого покемона грузить. Надо тут сделать т.н. постраничную подгрузку:
+      //чтоб сначала загрузились первые 30. Докрутили до низа - ещё 30, и т.д. И добавлять подгруженных в тот же массив
       let newPokemonsURL = "https://pokeapi.co/api/v2/pokemon?limit=\(self.counter)&offset=0"
       print(newPokemonsURL)
       self.url = URL(string: newPokemonsURL)!
--- PokemonLibraryTestTask/ViewController.swift
+++ PokemonLibraryTestTask/ViewController.swift
@@ -8,26 +8,31 @@ import UIKit
 import Kingfisher
 
 class ViewController: UIViewController {
-  
+
   let pokemonImageView = UIImageView()
   let pokemonLabelView = UILabel()
+  //опять же, force unwrap - это небезопасно. Забудешь проставить эти поля - будет крэш.
+  //нужно сделать их не-optional и создать с ними инициализатор, типа init(pokemonImage: URL, ...
   var pokemonImage: URL!
   var pokemonLabel: String!
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
+    //тут то же замечание, что и в TableViewController: код в методе отвечает за несколько задач.
+    //Надо создать свой отдельный класс PokeponView, вынести в него всё что касается constraint'ов и проставить PokeponView
+    //этому ViewController'у
     view.addSubview(pokemonImageView)
     view.addSubview(pokemonLabelView)
-    
+
     self.navigationItem.title = pokemonLabel;
-    
+
     pokemonImageView.translatesAutoresizingMaskIntoConstraints = false
     pokemonImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     pokemonImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     pokemonImageView.widthAnchor.constraint(equalToConstant: 250).isActive = true
     pokemonImageView.heightAnchor.constraint(equalToConstant: 250).isActive = true
     pokemonImageView.kf.setImage(with: pokemonImage)
-    
+
     pokemonLabelView.translatesAutoresizingMaskIntoConstraints = false
     pokemonLabelView.topAnchor.constraint(equalTo: pokemonImageView.bottomAnchor, constant: 4).isActive = true
     pokemonLabelView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
@@ -36,14 +41,14 @@ class ViewController: UIViewController {
     pokemonLabelView.textAlignment = .center
     pokemonLabelView.text = pokemonLabel
   }
-  
+
   func setImageName(image: URL){
     pokemonImage = image
   }
-  
+
   func setLabelName(name: String){
     pokemonLabel = name
   }
-  
+
 }
 
